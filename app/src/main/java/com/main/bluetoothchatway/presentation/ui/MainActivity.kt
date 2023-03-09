22d498dfc0e68c246b43c7c181186507c3be8621
vadwwxz <vadwwxz@gmail.com>
2023-03-09 10:00:39 +0200

    Initialized new module - core

--- app/src/main/java/com/main/bluetoothchatway/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/main/bluetoothchatway/presentation/ui/MainActivity.kt
@@ -1,9 +1,13 @@
-package com.main.bluetoothchatway
+package com.main.bluetoothchatway.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.main.bluetoothchatway.R
+import com.main.bluetoothchatway.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
