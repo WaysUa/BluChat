0ad5f4dd758746933c5bf358b79c8005b514f28d
vadwwxz <vadwwxz@gmail.com>
2023-03-10 10:22:09 +0200

    Created fragment_books.xml design, updated themes, renamed 1 fragment

--- chats/src/main/java/com/main/chats/presentation/ui/ChatsFragment.kt
+++ chats/src/main/java/com/main/chats/presentation/ui/ChatsFragment.kt
@@ -1,19 +1,22 @@
 package com.main.chats.presentation.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.main.chats.R
 import com.main.chats.databinding.FragmentBooksBinding
 import com.main.core.base.BaseFragment
 
-class BooksFragment : BaseFragment() {
+class ChatsFragment : BaseFragment() {
     private val binding by lazy { FragmentBooksBinding.inflate(layoutInflater) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ) = binding.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+    }
 }
